187a11856136d4742627b69d39b673d0bf69bd3e
jmej <jmejia@anestheticaudio.com>
2022-01-25 10:48:17 -0800

    first commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+open a terminal
+
+navigate to this project
+cd ~/Desktop/creative-coding-2/steep-and-fast
+
---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(800, 800);
+}
+
+function draw() {
+  background(255,0,0);
+  
+  ellipse(mouseX, mouseY, 20, 20);
+}
